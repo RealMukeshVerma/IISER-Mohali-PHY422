c058b106e98dd5d5ad3e87078a6658379ce200ce
RealMukeshVerma <MukeshVerma2001a@gmail.com>
2026-08-13 23:58:40 +0530

    Update error_calculation.py

--- error_calculation.py
+++ error_calculation.py
@@ -7,3 +7,16 @@ per_error=(rel_error)*100
 print(f"Absolute error : {absolute_error}")
 print(f"Relative error : {rel_error}")
 print(f"Percentage error : {per_error}")
+
+#for user input
+
+
+f=float(input("Inpute the actual value:"))
+e=float(input("Inpute the calculated value:"))
+g=f-e
+u_absolute_error=abs(g)
+u_rel_error=(u_absolute_error)/(e)
+u_per_error=(u_rel_error)*100
+print(f"Absolute error : {u_absolute_error}")
+print(f"Relative error : {u_rel_error}")
+print(f"Percentage error : {u_per_error}")
